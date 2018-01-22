7479575ac43427ce20a41ed3a6dfcce450c2309d
mirca <jvmirca@gmail.com>
2018-01-21 19:14:59 -0800

    we are online

--- CONTRIBUTING.md
+++
@@ -1,2 +0,0 @@
-lightkurve's contribution guidelines may be found at
-http://lightkurve.keplerscience.org/en/latest/contributing.html
--- lightkurve/version.py
+++
@@ -1,3 +0,0 @@
-# It is important to store the version number in a separate file
-# so that we can read it from setup.py without importing the package
-__version__ = "1.0.0dev"
