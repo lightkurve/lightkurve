f2c54b7ed46dbc16519d3f3f10e3456954216190
Geert Barentsen <geert@barentsen.be>
2018-02-13 12:30:07 -0800

    simplify the logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,7 +108,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = '_static/images/lightkurve.gif'
+html_logo = '_static/images/lightkurve.svg'
 
 
 # -- Options for HTMLHelp output ------------------------------------------
