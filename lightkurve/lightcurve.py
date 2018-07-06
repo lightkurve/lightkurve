94bfa456516d108473768bfac482836a0fc00029
Christina Hedges <christina.l.hedges@nasa.gov>
2018-07-06 14:32:27 -0700

    Added a fill_gaps method to LightCurve which fills in gaps in time and NaN values with linear intepolation (#184)
    * Added a fill_gaps function"
    
    * Added unit tests
    
    * Better tests
    
    * Whoops
    
    * Changes for @johnnyzhang295
    
    * Removed a whitespace

--- lightkurve/lightcurve.py
+++ lightkurve/lightcurve.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import os
 import datetime
 import logging
+import pandas as pd
 
 import oktopus
 import numpy as np
@@ -354,6 +355,45 @@ class LightCurve(object):
             A new ``LightCurve`` from which NaNs fluxes have been removed.
         """
         return self[~np.isnan(self.flux)]  # This will return a sliced copy
+
+    def fill_gaps(lc, method='nearest'):
+        """Fill in gaps in time with linear interpolation.
+
+        Parameters
+        ----------
+        method : string {None, 'backfill'/'bfill', 'pad'/'ffill', 'nearest'}
+            Method to use for gap filling. 'nearest' by default.
+
+        Returns
+        -------
+        nlc : LightCurve object
+            A new ``LightCurve`` in which NaNs values and gaps in time have been
+            filled.
+        """
+        clc = copy.deepcopy(lc.remove_nans())
+        nlc = copy.deepcopy(lc)
+
+        # Average gap between cadences
+        dt = np.nanmedian(clc.time[1::] - clc.time[:-1:])
+
+        # Iterate over flux and flux_err
+        for idx, y in enumerate([clc.flux, clc.flux_err]):
+            ts = pd.Series(y, index=clc.time)
+            newindex = [clc.time[0]]
+            for t in clc.time[1::]:
+                prevtime = newindex[-1]
+                while (t - prevtime) > 1.2*dt:
+                    newindex.append(prevtime + dt)
+                    prevtime = newindex[-1]
+                newindex.append(t)
+            ts = ts.reindex(newindex, method=method)
+            if idx == 0:
+                nlc.flux = np.asarray(ts)
+            elif idx == 1:
+                nlc.flux_err = np.asarray(ts)
+
+        nlc.time = np.asarray(ts.index)
+        return nlc
 
     def remove_outliers(self, sigma=5., return_mask=False, **kwargs):
         """Removes outlier flux values using sigma-clipping.
