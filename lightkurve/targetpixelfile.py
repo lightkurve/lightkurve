61e7c9b5853570ac0f01e6afff430a90144da940
Ryan Ridden-Harper <ryanr@Ryans-MacBook-Pro-4.local>
2018-10-04 17:56:11 +0930

    missed a call in the targetpixelfile class

--- lightkurve/targetpixelfile.py
+++ lightkurve/targetpixelfile.py
@@ -1255,7 +1255,9 @@ class TessTargetPixelFile(TargetPixelFile):
     @property
     def astropy_time(self):
         """Returns an AstroPy Time object for all good-quality cadences."""
-        return btjd_to_astropy_time(btjd=self.time)
+        timecorr = self.hdu[1].data['TIMECORR'][self.quality_mask]
+        return bkjd_to_astropy_time(bkjd=self.time, bkjdcorr=timecorr, 
+            timeslice=self.hdu[1].header['TIMSLICE'])
 
     def aperture_photometry(self, aperture_mask='pipeline'):
         """Performs aperture photometry.
