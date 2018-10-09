8368a16d975946ff85d2f30e7d69ec4b530bb683
Christina Hedges <christina.l.hedges@nasa.gov>
2018-10-09 11:40:26 -0700

    Added a unit test for BLS Kepler-10

--- lightkurve/tests/test_bls.py
+++ lightkurve/tests/test_bls.py
@@ -1,3 +1,22 @@
 from __future__ import division, print_function
 import pytest
 from bls import BLS
+import numpy as np
+from ..lightcurvefile import KeplerLightCurveFile
+
+
+KEPLER10 = ("https://archive.stsci.edu/missions/kepler/lightcurves/"
+            "0119/011904151/kplr011904151-2010009091648_llc.fits")
+
+
+@pytest.mark.remote_data
+def test_kepler_10():
+    """Make sure you can recover kepler 10"""
+    lcf = KeplerLightCurveFile(KEPLER10)
+    # Clip out the outliers
+    lc = lcf.PDCSAP_FLUX
+    temp_lc = lc.remove_outliers(10).flatten().remove_outliers(6)
+    lc =  lc[np.in1d(lcf.time, temp_lc.time)].flatten()
+    # Find the BLS power spectrum
+    model = BLS(lc.time, lc.flux).power(np.linspace(0.7, 0.9, 1000), np.linspace(0.001, 0.15, 10))
+    assert np.isclose(model.period[np.argmax(model.power)], 0.837537, rtol=1e-4)
