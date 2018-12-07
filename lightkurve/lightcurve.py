b70f04613a184dd6f72036ac67d8d65a71b60bc9
Christina Hedges <christina.l.hedges@nasa.gov>
2018-12-07 14:24:18 -0800

    Added an iterative feature to flatten to iteratively clip outliers when flattening.  (#362)
    * added iterative flatten
    
    * increased default iterations from 2 to 3
    
    * Added a user mask
    
    * Flipped mask
    
    * Get rid of some spaces

--- lightkurve/lightcurve.py
+++ lightkurve/lightcurve.py
@@ -11,7 +11,9 @@ import warnings
 
 import numpy as np
 from scipy import signal
+from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
+from copy import deepcopy
 
 from astropy.stats import sigma_clip
 from astropy.table import Table
@@ -257,7 +259,7 @@ class LightCurve(object):
         return copy.deepcopy(self)
 
     def flatten(self, window_length=101, polyorder=2, return_trend=False,
-                break_tolerance=5, **kwargs):
+                break_tolerance=5, niters=3, sigma=3, mask=None, **kwargs):
         """Removes the low frequency trend using scipy's Savitzky-Golay filter.
 
         This method wraps `scipy.signal.savgol_filter`.
@@ -279,6 +281,16 @@ class LightCurve(object):
             individually. A gap is defined as a period in time larger than
             `break_tolerance` times the median gap.  To disable this feature,
             set `break_tolerance` to None.
+        niters : int
+            Number of iterations to iteratively sigma clip and flatten. If more than one, will
+            perform the flatten several times, removing outliers each time.
+        sigma : int
+            Number of sigma above which to remove outliers from the flatten
+        mask : boolean array with length of self.time
+            Boolean array to mask data with before flattening. Flux values where
+            mask is True will not be used to flatten the data. An interpolated
+            result will be provided for these points. Use this mask to remove
+            data you want to preserve, e.g. transits.
         **kwargs : dict
             Dictionary of arguments to be passed to `scipy.signal.savgol_filter`.
 
@@ -290,39 +302,53 @@ class LightCurve(object):
         trend_lc : LightCurve object
             Trend in the lightcurve data
         """
-        if break_tolerance is None:
-            break_tolerance = np.nan
-        if polyorder >= window_length:
-            polyorder = window_length - 1
-            log.warning("polyorder must be smaller than window_length, "
-                        "using polyorder={}.".format(polyorder))
-        lc_clean = self.remove_nans()
-        # Split the lightcurve into segments by finding large gaps in time
-        dt = lc_clean.time[1:] - lc_clean.time[0:-1]
-        with warnings.catch_warnings():  # Ignore warnings due to NaNs
-            warnings.simplefilter("ignore", RuntimeWarning)
-            cut = np.where(dt > break_tolerance * np.nanmedian(dt))[0] + 1
-        low = np.append([0], cut)
-        high = np.append(cut, len(lc_clean.time))
-        # Then, apply the savgol_filter to each segment separately
-        trend_signal = np.zeros(len(lc_clean.time))
-        for l, h in zip(low, high):
-            # Reduce `window_length` and `polyorder` for short segments;
-            # this prevents `savgol_filter` from raising an exception
-            # If the segment is too short, just take the median
 
-            if np.any([window_length > (h - l), (h - l) < break_tolerance]):
-                trend_signal[l:h] = np.nanmedian(lc_clean.flux[l:h])
-            else:
-                # Scipy outputs a warning here that is not useful, will be fixed in version 1.2
-                with warnings.catch_warnings():
-                    warnings.simplefilter('ignore', FutureWarning)
-                    trend_signal[l:h] = signal.savgol_filter(x=lc_clean.flux[l:h],
-                                                             window_length=window_length,
-                                                             polyorder=polyorder,
-                                                             **kwargs)
+        if mask is None:
+            mask = np.ones(len(self.time), dtype=bool)
+        else:
+            # Deep copy ensures we don't change the original.
+            mask = deepcopy(~mask)
+        # No NaNs
+        mask &= np.isfinite(self.flux)
+        # No outliers
+        mask &= np.nan_to_num(np.abs(self.flux - np.nanmedian(self.flux))) <= (np.nanstd(self.flux) * sigma)
+        for iter in np.arange(0, niters):
+            if break_tolerance is None:
+                break_tolerance = np.nan
+            if polyorder >= window_length:
+                polyorder = window_length - 1
+                log.warning("polyorder must be smaller than window_length, "
+                            "using polyorder={}.".format(polyorder))
+            # Split the lightcurve into segments by finding large gaps in time
+            dt = self.time[mask][1:] - self.time[mask][0:-1]
+            with warnings.catch_warnings():  # Ignore warnings due to NaNs
+                warnings.simplefilter("ignore", RuntimeWarning)
+                cut = np.where(dt > break_tolerance * np.nanmedian(dt))[0] + 1
+            low = np.append([0], cut)
+            high = np.append(cut, len(self.time[mask]))
+            # Then, apply the savgol_filter to each segment separately
+            trend_signal = np.zeros(len(self.time[mask]))
+            for l, h in zip(low, high):
+                # Reduce `window_length` and `polyorder` for short segments;
+                # this prevents `savgol_filter` from raising an exception
+                # If the segment is too short, just take the median
+                if np.any([window_length > (h - l), (h - l) < break_tolerance]):
+                    trend_signal[l:h] = np.nanmedian(self.flux[mask][l:h])
+                else:
+                    # Scipy outputs a warning here that is not useful, will be fixed in version 1.2
+                    with warnings.catch_warnings():
+                        warnings.simplefilter('ignore', FutureWarning)
+                        trend_signal[l:h] = signal.savgol_filter(x=self.flux[mask][l:h],
+                                                                 window_length=window_length,
+                                                                 polyorder=polyorder,
+                                                                 **kwargs)
+            # No outliers
+            mask1 = np.nan_to_num(np.abs(self.flux[mask] - trend_signal)) <\
+                    (np.nanstd(self.flux[mask] - trend_signal) * sigma)
+            f = interp1d(self.time[mask][mask1], trend_signal[mask1], fill_value='extrapolate')
+            trend_signal = f(self.time)
+            mask[mask] &= mask1
 
-        trend_signal = np.interp(self.time, lc_clean.time, trend_signal)
         flatten_lc = self.copy()
         with warnings.catch_warnings():
             # ignore invalid division warnings
--- lightkurve/tests/test_lightcurve.py
+++ lightkurve/tests/test_lightcurve.py
@@ -591,6 +591,26 @@ def test_flatten_robustness():
     assert_allclose(lc.flux, flat_lc.flux * trend_lc.flux)
 
 
+def test_iterative_flatten():
+    '''Test the iterative sigma clipping in flatten '''
+    # Test a light curve with a single, buried outlier.
+    x = np.arange(2000)
+    y = np.sin(x/200)/100 + 1
+    y[250] -= 0.01
+    lc = LightCurve(x, y)
+    # Flatten it
+    c, f = lc.flatten(window_length=25, niters=2, sigma=3, return_trend=True)
+    # Only one outlier should remain.
+    assert np.isclose(c.flux, 1, rtol=0.00001).sum() == 1999
+    mask = np.zeros(2000, dtype=bool)
+    mask[250] = True
+    # Flatten it using a mask to remove the bad data point.
+    c, f = lc.flatten(window_length=25, niters=1, sigma=3, mask=mask,
+                      return_trend=True)
+    # Only one outlier should remain.
+    assert np.isclose(c.flux, 1, rtol=0.00001).sum() == 1999
+
+
 def test_fill_gaps():
     lc = LightCurve([1,2,3,4,6,7,8], [1,1,1,1,1,1,1])
     nlc = lc.fill_gaps()
