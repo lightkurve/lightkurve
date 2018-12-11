910744129006e8b391524c5709d221a7f3f485fa
Geert Barentsen <geert@barentsen.be>
2018-12-10 19:22:44 -0800

    Initial commit of kurvian-based docs

---
+++ docs/source/api.rst
@@ -0,0 +1,14 @@
+.. _api:
+
+*************
+API Reference
+*************
+
+.. toctree::
+   :maxdepth: 2
+
+   api/lightcurve
+   api/lightcurvefile
+   api/targetpixelfile
+   api/search
+   api/correctors
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,22 +124,18 @@ nbsphinx_prolog = r"""
 #nbsphinx_epilog = r""" text here """
 
 # -- Options for HTML output ----------------------------------------------
+html_theme = 'kurvian'
+html_theme_path = ['../']
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    "navbar_links": [
+        ("Quickstart", "quickstart"),
+        ("API", "api"),
+        ("Tutorials", "tutorials/index"),
+        ("About", "about"),
+    ],
+}
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 html_logo = '_static/images/lightkurve.png'
