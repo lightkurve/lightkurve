9c5a188d4f90bfda088b976ca428108bea33780d
mirca <jvmirca@gmail.com>
2019-01-26 12:57:24 +0800

    add some unit tests

--- lightkurve/prf/tests/test_prfmodel.py
+++ lightkurve/prf/tests/test_prfmodel.py
@@ -8,11 +8,11 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 
-from ...prf import KeplerPRF, SimpleKeplerPRF
+from ...prf import KeplerPRF, TessPRF
 from ...targetpixelfile import KeplerTargetPixelFile
 
 
-def test_prf_normalization():
+def test_kepler_prf_normalization():
     """Does the PRF model integrate to the requested flux across the focal plane?"""
     for channel in [1, 20, 40, 60, 84]:
         for col in [123, 678]:
@@ -24,10 +24,34 @@ def test_prf_normalization():
                 assert np.isclose(prf_sum, flux, rtol=0.1)
 
 
-def test_simple_kepler_prf():
+def test_tess_prf_normalization():
+    """Does the PRF model integrate to the requested flux across the focal plane?"""
+    for camera in [1, 2, 3, 4]:
+        for ccd in [1, 2, 3, 4]:
+            for col in [123, 678]:
+                for row in [234, 789]:
+                    shape = (11, 7)
+                    flux = 100
+                    prf = TessPRF(camera=camera, ccd=ccd, column=col, row=row, shape=shape)
+                    prf_sum = prf.evaluate(col + shape[0]/2, row + shape[1]/2, flux, 1, 1, 0).sum()
+                    assert np.isclose(prf_sum, flux, rtol=0.1)
+
+
+def test_kepler_prf():
     """Ensures that concentric PRFs have the same values."""
-    prf_1 = SimpleKeplerPRF(channel=16, shape=[20, 20], column=0, row=0)
-    prf_2 = SimpleKeplerPRF(channel=16, shape=[10, 10], column=5, row=5)
+    prf_1 = KeplerPRF(channel=16, shape=[20, 20], column=0, row=0)
+    prf_2 = KeplerPRF(channel=16, shape=[10, 10], column=5, row=5)
+    for c in [10, 8, 10, 7]:
+        for r in [10, 10, 7, 7]:
+            assert_allclose(prf_2(center_col=c, center_row=r, flux=1),
+                            prf_1(center_col=c, center_row=r, flux=1)[5:15, 5:15],
+                            rtol=1e-5)
+
+
+def test_tess_prf():
+    """Ensures that concentric PRFs have the same values."""
+    prf_1 = TessPRF(camera=2, ccd=3, shape=[20, 20], column=0, row=0)
+    prf_2 = TessPRF(camera=2, ccd=3, shape=[10, 10], column=5, row=5)
     for c in [10, 8, 10, 7]:
         for r in [10, 10, 7, 7]:
             assert_allclose(prf_2(center_col=c, center_row=r, flux=1),
@@ -36,10 +60,10 @@ def test_simple_kepler_prf():
 
 
 @pytest.mark.remote_data
-def test_simple_kepler_prf_interpolation_consistency():
+def test_kepler_prf_interpolation_consistency():
     """Ensures that the interpolated prf is consistent with calibration files.
     """
-    sprf = SimpleKeplerPRF(channel=56, shape=[15, 15], column=0, row=0)
+    sprf = KeplerPRF(channel=56, shape=[15, 15], column=0, row=0)
     cal_prf = fits.open("http://archive.stsci.edu/missions/kepler/fpc/prf/"
                         "extracted/kplr16.4_2011265_prf.fits")
     cal_prf_subsampled = cal_prf[-1].data[25::50, 25::50]
@@ -61,17 +85,6 @@ def test_get_model_prf():
     assert prf.shape == prf_from_tpf.shape
     assert prf.column == prf_from_tpf.column
     assert prf.row == prf_from_tpf.row
-
-def test_keplerprf_gradient_against_simplekeplerprf():
-    """is the gradient of KeplerPRF consistent with
-    the gradient of SimpleKeplerPRF?
-    """
-    kwargs = {'channel': 56, 'shape': [15, 15], 'column': 0, 'row': 0}
-    params = {'center_col': 7, 'center_row': 7, 'flux': 1.}
-    simple_prf = SimpleKeplerPRF(**kwargs)
-    prf = KeplerPRF(**kwargs)
-    prf_grad = prf.gradient(rotation_angle=0., scale_col=1., scale_row=1., **params)
-    assert_allclose(prf_grad[:-3], simple_prf.gradient(**params))
 
 
 @pytest.mark.parametrize("param_to_test", [("center_col"), ("center_row"), ("flux"),
@@ -95,4 +108,4 @@ def test_keplerprf_gradient_against_calculus(param_to_test):
     # compute analytical gradient
     prf_grad = prf.gradient(**params)
     # assert that the average absolute/relative error is less than 1e-5
-    assert np.max(np.abs(prf_grad[param_order[param_to_test]] - diff_prf) / (1. + np.abs(diff_prf))) < 1e-5
+    assert np.mean(np.abs(prf_grad[param_order[param_to_test]] - diff_prf) / (1. + np.abs(diff_prf))) < 1e-5
