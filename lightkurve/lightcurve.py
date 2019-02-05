3df45fb0ae0e78a6a366587ee4b40fd91b4ca422
Geert Barentsen <geert@barentsen.be>
2019-02-05 08:05:32 -0800

    Add LightCurve._repr_html_()

--- lightkurve/lightcurve.py
+++ lightkurve/lightcurve.py
@@ -1359,6 +1359,12 @@ class KeplerLightCurve(LightCurve):
     def __repr__(self):
         return('KeplerLightCurve(ID: {})'.format(self.targetid))
 
+    def _repr_html_(self):
+        """HTML representation which will display in notebooks by default."""
+        html = "<p>{}</p>".format(self.__repr__())
+        html += self.to_pandas().reset_index(drop=True).to_html(max_rows=6, notebook=True)
+        return html
+
     def correct(self, method='sff', **kwargs):
         """DEPRECATED: use `to_corrector(method).correct()` instead.
 
