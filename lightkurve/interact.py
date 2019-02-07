b6242f9b4217bd587d9808794c9b385ea75b83e3
Christina Hedges <christina.l.hedges@nasa.gov>
2019-02-07 15:30:55 -0700

    Update lightkurve/interact.py
    Co-Authored-By: gully <igully@gmail.com>

--- lightkurve/interact.py
+++ lightkurve/interact.py
@@ -217,7 +217,7 @@ def add_gaia_figure_elements(tpf, fig, magnitude_limit=18):
     radecs = np.vstack([result['RA_ICRS'], result['DE_ICRS']]).T
     coords = tpf.wcs.all_world2pix(radecs, 0) ## TODO, is this supposed to be zero or one?????
     year = ((tpf.astropy_time[0].jd - 2457206.375) * u.day).to(u.year)
-    pmra = ((np.asarray(result.pmRA) * u.milliarcsecond/u.year) * year).to(u.arcsec).value
+    pmra = ((np.nan_to_num(np.asarray(result.pmRA)) * u.milliarcsecond/u.year) * year).to(u.arcsec).value
     pmdec = ((np.asarray(result.pmDE) * u.milliarcsecond/u.year) * year).to(u.arcsec).value
     ## todo: filter NaNs in pmra/pmdec
     result.RA_ICRS += pmra
