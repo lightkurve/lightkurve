a20cc616bb513e0ca411e90b05b4d064337b98b0
Geert Barentsen <geert@barentsen.be>
2019-02-14 11:23:57 -0500

    Update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0b27',
+    VERSION: '1.0b29',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
