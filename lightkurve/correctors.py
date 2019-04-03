a6169d4e125ee0039ff7074277de290f3d96139d
Christina Hedges <christina.l.hedges@nasa.gov>
2019-04-02 19:23:05 -0700

    [bugfix] Fix PLD NaN bug (#488)
    * bugfix
    
    * removing extra white space

--- lightkurve/correctors.py
+++ lightkurve/correctors.py
@@ -690,7 +690,9 @@ class PLDCorrector(object):
         # first order PLD design matrix
         pld_flux = flux_crop[:, aperture_crop]
         f1 = np.reshape(pld_flux, (len(pld_flux), -1))
-        X1 = f1 / np.sum(pld_flux, axis=-1)[:, None]
+        X1 = f1 / np.nansum(pld_flux, axis=-1)[:, None]
+        # No NaN pixels
+        X1 = X1[:, np.isfinite(X1).all(axis=0)]
 
         # higher order PLD design matrices
         X_sections = [np.ones((len(flux_crop), 1)), X1]
