1e4abe18070e39a010ec7f115caab445ef2d9ea9
Ann Marie Cody <annmarie.cody@gmail.com>
2019-04-05 16:33:09 -0700

    Corrected 1CRV5P,2CRV5P values so that they are the lower left TPF corner in *detector* coordinates

--- lightkurve/targetpixelfile.py
+++ lightkurve/targetpixelfile.py
@@ -1143,10 +1143,23 @@ class KeplerTargetPixelFile(TargetPixelFile):
                                   size=size, mode='partial')
             factory.add_cadence(frameno=idx, flux=cutout.data, header=hdu.header)
 
-        ext_info = {'1CRV5P': column, '2CRV5P': row}
+        ext_info = {}
         ext_info['TFORM4'] = '{}J'.format(size[0] * size[1])
         ext_info['TDIM4'] = '({},{})'.format(size[0], size[1])
         ext_info.update(cutout.wcs.to_header())
+
+        # TPF contains multiple data columns that require WCS
+        for m in [4, 5, 6, 7, 8, 9]:
+            if m > 4:
+                ext_info["TFORM{}".format(m)] = '{}E'.format(size[0] * size[1])
+                ext_info['TDIM{}'.format(m)] = '({},{})'.format(size[0], size[1])
+            # Compute location of TPF lower left corner
+            # Int statement contains modulus so that .5 values always round up.
+            # We cannot use numpy.round() as it rounds to the even number.
+            half_tpfsize_col = int((size[0] - 1) / 2 + (size[0] + 1) % 2)
+            half_tpfsize_row = int((size[1] - 1) / 2 + (size[1] + 1) % 2)
+            ext_info['1CRV{}P'.format(m)] = column - half_tpfsize_col + factory.keywords['CRVAL1P']
+            ext_info['2CRV{}P'.format(m)] = row - half_tpfsize_row + factory.keywords['CRVAL2P']
 
         return factory.get_tpf(hdu0_keywords=allkeys, ext_info=ext_info, **kwargs)
 
