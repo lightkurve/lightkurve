Fix PLD masking problem (#498)
* fix

* fix

* whoops

* whoops2

lightkurve/correctors.py
@@ -718,8 +718,8 @@ class PLDCorrector(object):
 
         # set default transit mask
         if cadence_mask is None:
-            cadence_mask = np.ones_like(self.time, dtype=bool)
-        M = lambda x: x[cadence_mask]
+            cadence_mask = np.ones_like(lc.time, dtype=bool)
+        M = lambda x: x[cadence_mask[nanmask]]
 
         # mask transits in design matrix
         MX = M(X)