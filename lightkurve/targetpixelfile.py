3a1195fb50df69894024e36a48d2ab03760831d8
Christina Hedges <christina.l.hedges@nasa.gov>
2019-07-17 15:45:53 -0700

    Added the cutout method to the TargetPixelFile class (#537)
    * Fixed split campaign bug
    
    * slightly more robust
    
    * remove print statement
    
    * apply to LCs
    
    * Added unit test
    
    * Added rolling band tutorial
    
    * added cutout method
    
    * Remove accidental ipynb file
    
    * Improve docstring, unit tests, and fix Python 2 compatibility.
    
    * Fix compatibility with Python 2, take 3
    
    * Fix compatibility with Python 2, take 4
    
    * docstring improvements
    
    * Ensure center and size are in (col, row) format
    
    * Attempt to fix unit test

--- lightkurve/targetpixelfile.py
+++ lightkurve/targetpixelfile.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.ndimage import label
 from tqdm import tqdm
+from copy import deepcopy
 from astropy.coordinates import SkyCoord
 from astropy.stats.funcs import median_absolute_deviation as MAD
 
@@ -693,6 +694,140 @@ class TargetPixelFile(object):
         if method == "pld":
             from .correctors import PLDCorrector
             return PLDCorrector(self)
+
+
+    def cutout(self, center=None, size=5):
+        """Cut a rectangle out of the Target Pixel File.
+
+        This methods returns a new `TargetPixelFile` object containing a
+        rectangle of a given ``size`` cut out around a given ``center``.
+
+        Parameters
+        ----------
+        center : (int, int) tuple or `astropy.SkyCoord`
+            Center of the cutout.  If an (int, int) tuple is passed, it wil be
+            interpreted as the (column, row) coordinates relative to
+            the bottom-left corner of the TPF.  If an `astropy.SkyCoord` is
+            passed then the sky coordinate will be used instead.
+            If `None` (default) then the center of the TPF will be used.
+        size : int or (int, int) tuple
+            Number of pixels to cut out. If a single integer is passed then
+            a square of that size will be cut. If a tuple is passed then a
+            rectangle with dimensions (column_size, row_size) will be cut.
+
+        Returns
+        -------
+        tpf : `lightkurve.TargetPixelFile` object
+            New and smaller Target Pixel File object containing only the data
+            cut out.
+        """
+        imshape = self.flux.shape[1:]
+
+        # Parse the user input (``center``) into an (x, y) coordinate
+        if center is None:
+            x, y = imshape[0]//2, imshape[1]//2
+        elif isinstance(center, SkyCoord):
+            try:
+                x, y = self.wcs.world_to_pixel(center)
+            except AttributeError:
+                # Python 2 compatibility (i.e. syntax of older AstroPy versions)
+                x, y = self.wcs.all_world2pix([[center.ra.value, center.dec.value]], 1)[0]
+        elif isinstance(center, (tuple, list, np.ndarray)):
+            x, y = center
+        col = int(x)
+        row = int(y)
+
+        # Parse the user input (``size``)
+        if isinstance(size, int):
+            s = (size/2, size/2)
+        elif isinstance(size, (tuple, list, np.ndarray)):
+            s = (size[0]/2, size[1]/2)
+
+        # Find the TPF edges
+        col_edges = np.asarray([np.max([0, col-s[0]]),
+                                np.min([col+s[0], imshape[1] - 1])],
+                               dtype=int)
+        row_edges = np.asarray([np.max([0, row-s[1]]),
+                                np.min([row+s[1], imshape[0] - 1])],
+                               dtype=int)
+
+        # Make a copy of the data extension
+        hdu = self.hdu[0].copy()
+
+        # Find the new object coordinates
+        r, d = self.get_coordinates(cadence=len(self.flux)//2)
+        hdu.header['RA_OBJ'] = np.nanmean(r[row_edges[0]:row_edges[1], col_edges[0]:col_edges[1]])
+        hdu.header['DEC_OBJ'] = np.nanmean(d[row_edges[0]:row_edges[1], col_edges[0]:col_edges[1]])
+
+        # Remove any KIC labels
+        labels = ['*MAG', 'PM*', 'GL*', 'OBJECT', 'PARALLAX', '*COLOR', 'TEFF',
+                  'LOGG', 'FEH', 'EBMINUSV', 'AV', "RADIUS", "TMINDEX", "OBJECT"]
+        for label in labels:
+            if label in hdu.header:
+                hdu.header[label] = fits.card.Undefined()
+
+        keys = np.asarray([k for k in self.header.keys()])
+        if 'KEPLERID' in keys:
+            hdu.header['KEPLERID'] = '{}{}'.format(hdu.header['KEPLERID'], '_CUTOUT')
+        if 'TICID' in keys:
+            hdu.header['TICID'] = '{}{}'.format(hdu.header['TICID'], '_CUTOUT')
+
+        # HDUList
+        hdus = [hdu]
+
+        # Copy the header
+        hdr = deepcopy(self.hdu[1].header)
+
+        # Trim any columns that have the shape of the image, to be the new shape
+        data_columns = []
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            for idx, datacol in enumerate(self.hdu[1].columns):
+                # If the column is 3D
+                if (len(self.hdu[1].data[datacol.name].shape) == 3):
+                    # Make a copy, trim it and change the format
+                    datacol = deepcopy(datacol)
+                    datacol.array = datacol.array[:, row_edges[0]:row_edges[1], col_edges[0]:col_edges[1]]
+                    datacol._dim = '{}'.format(datacol.array.shape[1:]).replace(' ', '')
+                    datacol._dims = datacol.array.shape[1:]
+                    datacol._format = fits.column._ColumnFormat('{}{}'.format(np.product(datacol.array.shape[1:]),
+                                                                              datacol._format[-1]))
+                    data_columns.append(datacol)
+                    hdr['TDIM{}'.format(idx)] = '{}'.format(datacol.array.shape[1:]).replace(' ', '')
+                    hdr['TDIM9'] = '{}'.format(datacol.array.shape[1:]).replace(' ', '')
+                    hdr['TDIM13'] = '{}'.format((0, datacol.array.shape[1])).replace(' ', '')
+                else:
+                    data_columns.append(datacol)
+
+        # Get those coordinates sorted for the corner of the TPF and the WCS
+        hdr['1CRV*P'] = hdr['1CRV4P'] + col_edges[0]
+        hdr['2CRV*P'] = hdr['2CRV4P'] + row_edges[0]
+        hdr['1CRPX*'] = hdr['1CRPX4'] - col_edges[0]
+        hdr['2CRPX*'] = hdr['2CRPX4'] - row_edges[0]
+
+        # Make a table for the data
+        data_columns[-1]._dim = '{}'.format((0, int(data_columns[5]._dim.split(',')[1][:-1]))).replace(' ', '')
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            btbl = fits.BinTableHDU.from_columns(data_columns, header=hdr)
+
+        # Append it to the hdulist
+        hdus.append(btbl)
+
+        # Correct the aperture mask
+        hdu = self.hdu[2].copy()
+        ar = hdu.data
+        ar = ar[row_edges[0]:row_edges[1], col_edges[0]:col_edges[1]]
+        hdu.header['NAXIS1'] = ar.shape[0]
+        hdu.header['NAXIS2'] = ar.shape[1]
+        hdu.data = ar
+        hdus.append(hdu)
+
+        # Make a new tpf
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            newfits = fits.HDUList(hdus)
+        return self.__class__(newfits)
 
 
 class KeplerTargetPixelFile(TargetPixelFile):
@@ -1395,6 +1530,7 @@ class KeplerTargetPixelFileFactory(object):
                 hdu.header[keyword] = ""  # override wcs keywords
         hdu.header['EXTNAME'] = 'APERTURE'
         return hdu
+
 
 
 class TessTargetPixelFile(TargetPixelFile):
