939f889a817c44c1e54887b27851a629ec156c9b
Geert Barentsen <geert@barentsen.be>
2019-09-20 16:48:21 -0700

    Improve the unit test

--- lightkurve/correctors/tests/test_regressioncorrector.py
+++ lightkurve/correctors/tests/test_regressioncorrector.py
@@ -5,17 +5,15 @@ from ... import LightCurve
 from .. import RegressionCorrector, DesignMatrix
 
 
-def test_simple_example():
+def test_sinusoid_noise():
     """Can we remove simple sinusoid noise added to a flat light curve?"""
     size = 100
     time = np.linspace(1, 100, size)
-    true_flux = np.ones(size)
-    flux_err = 0.1*np.ones(size)
     noise = np.sin(time/5)
-    lc = LightCurve(time, true_flux+noise, flux_err)
-
-    dm = DesignMatrix({'noise': noise, 'offset': np.ones(len(noise))})
-    rc = RegressionCorrector(lc, dm)
-
+    true_lc = LightCurve(time, np.ones(size), flux_err=0.1*np.ones(size))
+    noisy_lc = LightCurve(time, true_lc.flux+noise, true_lc.flux_err)
+    dm = DesignMatrix({'noise': noise, 'offset': np.ones(len(time))},
+                      name='noise_model')
+    rc = RegressionCorrector(noisy_lc, dm)
     corrected_lc = rc.correct()
-    assert_almost_equal(corrected_lc.normalize().flux, true_flux)
+    assert_almost_equal(corrected_lc.normalize().flux, true_lc.flux)
