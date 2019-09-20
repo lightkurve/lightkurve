fc76ddfdff51410eda82a707064d84c7a404c9fe
Geert Barentsen <geert@barentsen.be>
2019-09-20 16:34:39 -0700

    Add a first unit test for RegressionCorrector

--- lightkurve/correctors/tests/test_regressioncorrector.py
+++ lightkurve/correctors/tests/test_regressioncorrector.py
@@ -1,3 +1,7 @@
+import numpy as np
+from numpy.testing import assert_array_equal, assert_almost_equal
+
+from ... import LightCurve
 from .. import RegressionCorrector, DesignMatrix
 
 
@@ -9,3 +13,19 @@ def test_regression_corrector():
     np.sin
     dm = DesignMatrix({})
     RegressionCorrector()
+
+
+def test_simple_example():
+    """Can we remove simple sinusoid noise added to a flat light curve?"""
+    size = 100
+    time = np.linspace(1, 100, size)
+    true_flux = np.ones(size)
+    flux_err = 0.1*np.ones(size)
+    noise = np.sin(time/5)
+    lc = LightCurve(time, true_flux+noise, flux_err)
+
+    dm = DesignMatrix({'noise': noise, 'offset': np.ones(len(noise))})
+    rc = RegressionCorrector(lc, dm)
+
+    corrected_lc = rc.correct()
+    assert_almost_equal(corrected_lc.normalize().flux, true_flux)
