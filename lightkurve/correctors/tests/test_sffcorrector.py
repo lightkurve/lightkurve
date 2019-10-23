8814d7d843c952b7b89007d878d4dc4f6e8f7158
Christina Hedges <christina.l.hedges@nasa.gov>
2019-10-23 15:48:02 -0700

    added test

--- lightkurve/correctors/tests/test_sffcorrector.py
+++ lightkurve/correctors/tests/test_sffcorrector.py
@@ -121,3 +121,105 @@ def test_sff_knots():
 
     sff = SFFCorrector(lc)
     sff.correct()  # should not raise an exception
+
+
+def test_sff_corrector():
+    """Does our code agree with the example presented in Vanderburg
+    and Johnson (2014)?"""
+    # The following csv file, provided by Vanderburg and Johnson
+    # at https://www.cfa.harvard.edu/~avanderb/k2/ep60021426.html,
+    # contains the results of applying SFF to EPIC 60021426.
+    fn = get_pkg_data_filename('../../tests/data/ep60021426alldiagnostics.csv')
+    data = np.genfromtxt(fn, delimiter=',', skip_header=1)
+    mask = data[:, -2] == 0  # indicates whether the thrusters were on or off
+    time = data[:, 0]
+    raw_flux = data[:, 1]
+    corrected_flux = data[:, 2]
+    centroid_col = data[:, 3]
+    centroid_row = data[:, 4]
+    arclength = data[:, 5]
+    correction = data[:, 6]
+
+    lc = LightCurve(time=time, flux=raw_flux, flux_err=np.ones(len(raw_flux)) * 0.0001)
+    sff = SFFCorrector(lc)
+    corrected_lc = sff.correct(centroid_col=centroid_col,
+                               centroid_row=centroid_row,
+                               restore_trend=True)
+    assert (np.isclose(corrected_flux, corrected_lc.flux, atol=0.001).all())
+
+    # masking
+    corrected_lc = sff.correct(centroid_col=centroid_col,
+                               centroid_row=centroid_row,
+                               windows=3,
+                               restore_trend=True,
+                               cadence_mask=mask)
+    assert (np.isclose(corrected_flux, corrected_lc.flux, atol=0.001).all())
+
+    # masking and breakindex
+    corrected_lc = sff.correct(centroid_col=centroid_col,
+                               centroid_row=centroid_row,
+                               windows=3,
+                               restore_trend=True,
+                               cadence_mask=mask,
+                               breakindex=150)
+    assert (np.isclose(corrected_flux, corrected_lc.flux, atol=0.001).all())
+
+    # masking and breakindex and iters
+    corrected_lc = sff.correct(centroid_col=centroid_col,
+                               centroid_row=centroid_row, windows=3, restore_trend=True,
+                               cadence_mask=mask, breakindex=150, niters=3)
+    assert (np.isclose(corrected_flux, corrected_lc.flux, atol=0.001).all())
+
+    # masking and breakindex and bins
+    corrected_lc = sff.correct(centroid_col=centroid_col,
+                               centroid_row=centroid_row, windows=3, restore_trend=True,
+                               cadence_mask=mask, breakindex=150, bins=5)
+    assert (np.isclose(corrected_flux, corrected_lc.flux, atol=0.001).all())
+    assert np.all((sff.lc.flux_err/sff.corrected_lc.flux_err) == 1)
+
+
+    # masking and breakindex and bins and propagate_errors
+    corrected_lc = sff.correct(centroid_col=centroid_col,
+                               centroid_row=centroid_row, windows=3, restore_trend=True,
+                               cadence_mask=mask, breakindex=150, bins=5, propagate_errors=True)
+    assert (np.isclose(corrected_flux, corrected_lc.flux, atol=0.001).all())
+    assert np.all((sff.lc.flux_err/sff.corrected_lc.flux_err) < 1)
+
+    # test using KeplerLightCurve interface
+    klc = KeplerLightCurve(time=time,
+                           flux=raw_flux,
+                           flux_err=np.ones(len(raw_flux)) * 0.0001,
+                           centroid_col=centroid_col,
+                           centroid_row=centroid_row)
+    sff = klc.to_corrector("sff")
+    klc = sff.correct(windows=3, restore_trend=True)
+    assert (np.isclose(corrected_flux, klc.flux, atol=0.001).all())
+
+    # Can plot
+    sff.diagnose()
+
+def test_sff_priors():
+    ''' SFF Spline flux mean should == lc.flux.mean()
+    SFF arclength component should have mean 0
+    '''
+    n_points = 300
+    fn = get_pkg_data_filename('../../tests/data/ep60021426alldiagnostics.csv')
+    data = np.genfromtxt(fn, delimiter=',', skip_header=1)
+    raw_flux = data[:, 1][:n_points]
+    centroid_col = data[:, 3][:n_points]
+    centroid_row = data[:, 4][:n_points]
+
+    time = np.concatenate((np.linspace(0, 20, int(n_points/3)),
+                           np.linspace(30, 78, int(n_points/3)),
+                           np.linspace(80, 100, int(n_points/3))
+                           ))
+    lc = KeplerLightCurve(time=time,
+                          flux=raw_flux,
+                          flux_err=np.ones(n_points) * 0.0001,
+                          centroid_col=centroid_col,
+                          centroid_row=centroid_row)
+
+    sff = SFFCorrector(lc)
+    sff.correct()  # should not raise an exception
+    assert np.isclose(sff.diagnostic_lightcurves['spline'].flux.mean(), lc.flux.mean(), atol=1e-4)
+    assert np.isclose(sff.diagnostic_lightcurves['sff'].flux.mean(), 0, atol=1e-4)
