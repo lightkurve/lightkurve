89a48301c21f33a5159147181e0152098158547c
Geert Barentsen <geert@barentsen.be>
2019-11-22 19:20:08 -0800

    Avoid forcing Agg as the matplotlib backend on OSX (#640)
    * Avoid forcing Agg as the matplotlib backend on OSX
    
    * Fix ImportError
    
    * Allow for empty string

--- lightkurve/__init__.py
+++ lightkurve/__init__.py
@@ -9,7 +9,8 @@ MPLSTYLE = '{}/data/lightkurve.mplstyle'.format(PACKAGEDIR)
 # By default Matplotlib is configured to work with a graphical user interface
 # which may require an X11 connection (i.e. a display).  When no display is
 # available, errors may occur.  In this case, we default to the robust Agg backend.
-if os.name == 'posix' and "DISPLAY" not in os.environ:
+import platform
+if platform.system() == "Linux" and os.environ.get('DISPLAY', '') == '':
     import matplotlib
     matplotlib.use('Agg')
 
