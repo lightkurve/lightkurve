7aa4b4085b182a8c30d9ed4a6623debf7bf4653b
Christina Hedges <christina.l.hedges@nasa.gov>
2020-04-08 11:50:20 -0700

    updating the API

--- lightkurve/correctors/designmatrix.py
+++ lightkurve/correctors/designmatrix.py
@@ -8,7 +8,8 @@ import warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.sparse import lil_matrix, hstack, vstack
+from scipy.sparse import lil_matrix, csr_matrix, hstack, vstack, issparse, find
+from copy import deepcopy
 
 from .. import MPLSTYLE
 from ..utils import LightkurveWarning, plot_image
@@ -28,11 +29,11 @@ class DesignMatrix():
 
     Parameters
     ----------
-    df : dict, array, or `pandas.DataFrame` object
+    X : dict, array, or `pandas.DataFrame` object
         Columns to include in the design matrix.  If this object is not a
         `~pandas.DataFrame` then it will be passed to the DataFrame constructor.
     columns : iterable of str (optional)
-        Column names, if not already provided via ``df``.
+        Column names, if not already provided via ``X``.
     name : str
         Name of the matrix.
     prior_mu : array
@@ -42,23 +43,39 @@ class DesignMatrix():
         Prior standard deviations of the coefficients associated with each
         column in a linear regression problem.
     """
-    def __init__(self, df, columns=None, name='unnamed_matrix', prior_mu=None,
+    def __init__(self, X, columns=None, name='unnamed_matrix', prior_mu=None,
                  prior_sigma=None, sparse=False):
-        if not isinstance(df, pd.DataFrame):
-            df = pd.DataFrame(df)
-        if columns is not None:
-            df.columns = columns
-        self.df = df
+
         self.name = name
         self._sparse = sparse
-        if self._sparse:
-            self._sparse_values = lil_matrix(self.df.values)
+
+        if issparse(X):
+            # Build a sparse DM
+            self._sparse = True
+            self.X = X
+            self.columns = columns
+        else:
+            if not isinstance(X, pd.DataFrame):
+                X = pd.DataFrame(X)
+            if columns is not None:
+                if hasattr(X, 'columns'):
+                    X.columns = columns
+                else:
+                    self.columns = columns
+            else:
+                self.columns = columns
+            if self._sparse:
+                self.X = lil_matrix(np.asarray(X))
+            else:
+                self.X = X
+
         if prior_mu is None:
-            prior_mu = np.zeros(len(df.T))
+            prior_mu = np.zeros(X.shape[1])
         if prior_sigma is None:
-            prior_sigma = np.ones(len(df.T)) * np.inf
+            prior_sigma = np.ones(X.shape[1]) * np.inf
         self.prior_mu = prior_mu
         self.prior_sigma = prior_sigma
+
 
     def plot(self, ax=None, **kwargs):
         """Visualize the design matrix values as an image.
@@ -81,15 +98,14 @@ class DesignMatrix():
         """
         with plt.style.context(MPLSTYLE):
             values = self.values
-            if self._sparse:
-                values = values.toarray()
             ax = plot_image(values, ax=ax, xlabel='Component', ylabel='X',
                             clabel='Component Value', title=self.name, **kwargs)
             ax.set_aspect(self.shape[1]/(1.6*self.shape[0]))
             if self.shape[1] <= 40:
                 ax.set_xticks(np.arange(self.shape[1]))
-                ax.set_xticklabels([r'${}$'.format(i) for i in self.columns],
-                                   rotation=90, fontsize=8)
+                if self.columns is not None:
+                    ax.set_xticklabels([r'${}$'.format(i) for i in self.columns],
+                                       rotation=90, fontsize=8)
         return ax
 
     def plot_priors(self, ax=None):
@@ -125,7 +141,7 @@ class DesignMatrix():
         """Returns a random sample from the prior distribution."""
         return np.random.normal(self.prior_mu, self.prior_sigma)
 
-    def split(self, row_indices):
+    def split(self, row_indices, inplace=False):
         """Returns a new `.DesignMatrix` with regressors split into multiple
         columns.
 
@@ -145,24 +161,38 @@ class DesignMatrix():
         `.DesignMatrix`
             A new design matrix with shape (n_rows, len(row_indices)*n_columns).
         """
-        if isinstance(row_indices, int):
-            row_indices = [row_indices]
-        if (len(row_indices) == 0) or (row_indices == [0]) or (row_indices is None):
+        if not hasattr(row_indices, '__iter__'):
+            row_indices = list(row_indices)
+        if len(row_indices) == 0:
             return self
-        # Where do the submatrices begin and end?
-        lower_idx = np.append(0, row_indices)
-        upper_idx = np.append(row_indices, len(self.df))
-        dfs = []
-        for idx, a, b in zip(range(len(lower_idx)), lower_idx, upper_idx):
-            new_columns = dict(
-                ('{}'.format(val), '{}'.format(val) + ' {}'.format(idx + 1))
-                for val in list(self.df.columns))
-            dfs.append(self.df[a:b].rename(columns=new_columns))
-        new_df = pd.concat(dfs, axis=1).fillna(0)
-        prior_mu = np.hstack([self.prior_mu for idx in range(len(dfs))])
-        prior_sigma = np.hstack([self.prior_sigma for idx in range(len(dfs))])
-        return DesignMatrix(new_df, name=self.name, prior_mu=prior_mu,
-                            prior_sigma=prior_sigma, sparse=self._sparse)
+        if (row_indices == [0]) or (row_indices == [self.shape[0]]):
+            return self
+
+        if inplace:
+            dm = self
+        else:
+            dm = self.copy()
+
+        x = np.arange(dm.shape[0])
+        dm.prior_mu = np.concatenate([list(self.prior_mu) * (len(row_indices) + 1)])
+        dm.prior_sigma = np.concatenate([list(self.prior_sigma) * (len(row_indices) + 1)])
+        if isinstance(dm.X, pd.DataFrame):
+            dm.X = pd.concat([((dm.X * np.atleast_2d(np.in1d(x, idx).astype(int)).T)).add_suffix('_{}'.format(jdx)) for jdx, idx in enumerate(np.array_split(x, row_indices))], axis=1)
+            non_zero = dm.X.sum(axis=0) != 0
+            dm.X = dm.X[dm.X.columns[non_zero]]
+        if issparse(dm.X):
+            dm.X = hstack([dm.X.multiply(lil_matrix(np.in1d(x, idx).astype(int)).T) for idx in np.array_split(x, row_indices)], format='lil')
+            non_zero = dm.X.sum(axis=0) != 0
+            non_zero = np.asarray(non_zero).ravel()
+            dm.X = dm.X[:, non_zero]
+
+        dm.prior_mu = dm.prior_mu[non_zero]
+        dm.prior_sigma = dm.prior_sigma[non_zero]
+        return dm
+
+    def copy(self):
+        """Returns a deepcopy of DesignMatrix"""
+        return deepcopy(self)
 
     def standardize(self, inplace=False):
         """Returns a new `.DesignMatrix` in which the columns have been
@@ -185,20 +215,32 @@ class DesignMatrix():
         `.DesignMatrix`
             A new design matrix with median-subtracted & sigma-divided columns.
         """
-        df_nan = self.df.replace(0, np.nan)
-        mean = df_nan.mean()
-        std = df_nan.std()
-        mean[std == 0] = 0
-        std[std == 0] = 1
-        df = ((df_nan - mean) / std).fillna(0)
-        if 'offset' in df.columns:
-            df['offset'] = 1
+
         if inplace:
-            self.df = df
-            if self._sparse:
-                self._sparse_values = lil_matrix(self.df.values)
-            return self
-        return DesignMatrix(df, name=self.name, sparse=self._sparse)
+            dm = self
+        else:
+            dm = self.copy()
+
+        if isinstance(dm.X, pd.DataFrame):
+            df_nan = dm.X.replace(0, np.nan)
+            mean = df_nan.mean()
+            std = df_nan.std()
+            mean[std == 0] = 0
+            std[std == 0] = 1
+            dm.X = ((df_nan - mean) / std).fillna(0)
+
+        if issparse(dm.X):
+            idx, jdx, v = find(dm.X)
+            weights = dm.X.copy()
+            weights[dm.X != 0] = 1
+            mean = np.bincount(jdx, weights=v)/np.bincount(jdx)
+            std = np.asarray([((np.sum((v[jdx == i] - mean[i])**2) * (1/((jdx == i).sum() - 1))))**0.5 for i in np.unique(jdx)])
+            mean[std == 0] = 0
+            std[std == 0] = 1
+            white = (dm.X - vstack([lil_matrix(mean)] * dm.shape[0])).multiply(vstack([lil_matrix(1/std)] * dm.shape[0]))
+            dm.X = white.multiply(weights)
+        return dm
+
 
     def pca(self, nterms=6):
         """Returns a new `.DesignMatrix` with a smaller number of regressors.
@@ -224,10 +266,7 @@ class DesignMatrix():
         # we find this to be too few, and that n_iter=10 is still fast but
         # produces more stable results.
         from fbpca import pca  # local import because not used elsewhere
-        if self._sparse:
-            new_values, _, _ = pca(self.values.toarray(), nterms, n_iter=10)
-        else:
-            new_values, _, _ = pca(self.values, nterms, n_iter=10)
+        new_values, _, _ = pca(self.values, nterms, n_iter=10)
         return DesignMatrix(new_values, name=self.name, sparse=self._sparse)
 
     def append_constant(self, prior_mu=0, prior_sigma=np.inf, inplace=False):
@@ -243,11 +282,12 @@ class DesignMatrix():
             dm = self
         else:
             dm = self.copy()
-        dm.df.insert(self.df.shape[1], 'offset', 1, allow_duplicates=False)
-        dm.prior_mu = np.append(self.prior_mu, prior_mu)
-        dm.prior_sigma = np.append(self.prior_sigma, prior_sigma)
-        if self._sparse:
-            self._sparse_values = hstack([self.values, lil_matrix(np.ones(self.values.shape[0])).T], format='lil')
+        if isinstance(dm.X, pd.DataFrame):
+            dm.X.insert(dm.shape[1], 'offset', 1, allow_duplicates=False)
+        else:
+            dm.X = hstack([dm.X, lil_matrix(np.ones(dm.shape[0])).T], format='lil')
+        dm.prior_mu = np.append(dm.prior_mu, prior_mu)
+        dm.prior_sigma = np.append(dm.prior_sigma, prior_sigma)
         return dm
 
     def _validate(self):
@@ -265,29 +305,22 @@ class DesignMatrix():
     @property
     def rank(self):
         """Matrix rank computed using `numpy.linalg.matrix_rank`."""
-        if self._sparse:
-                return np.linalg.matrix_rank(self.values.toarray())
         return np.linalg.matrix_rank(self.values)
-
-    @property
-    def columns(self):
-        """List of column names."""
-        return list(self.df.columns)
 
     @property
     def shape(self):
         """Tuple specifying the shape of the matrix as (n_rows, n_columns)."""
-        return self.df.shape
+        return self.X.shape
 
     @property
     def values(self):
         """2D numpy array containing the matrix values."""
-        if self._sparse:
-            return self._sparse_values
-        return self.df.values
+        if issparse(self.X):
+            return self.X.toarray()
+        return self.X.values
 
-    def __getitem__(self, key):
-        return self.df[key]
+    # def __getitem__(self, key):
+    #     return self.df[key]
 
     def __repr__(self):
         if self._sparse:
@@ -300,18 +333,22 @@ class DesignMatrixCollection():
     def __init__(self, matrices):
         if np.all([m._sparse for m in matrices]):
             self._sparse = True
-            self._sparse_values = hstack([m.values for m in matrices], format='lil')
+#            self._sparse_values = hstack([m.values for m in matrices], format='lil')
         else:
             self._sparse = False
-
         self.matrices = matrices
 
     @property
     def values(self):
         """2D numpy array containing the matrix values."""
-        if self._sparse:
-            return self._sparse_values
+#       if self._sparse:
+#            return self._sparse_values
         return np.hstack(tuple(m.values if isinstance(m.values, np.ndarray) else m.values.toarray() for m in self.matrices))
+
+
+    def X(self):
+        """Stack of X as either np.array or sparse array"""
+        return None
 
     @property
     def prior_mu(self):
