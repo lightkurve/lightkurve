751caea9ee8bbe1bfb81bf84f6d799d71964e5cd
Nicholas Saunders <nksaunders1994@gmail.com>
2020-06-11 16:28:42 -1000

    remove gp attempts

--- lightkurve/correctors/pldcorrector.py
+++ lightkurve/correctors/pldcorrector.py
@@ -83,6 +83,7 @@ class PLDCorrector(RegressionCorrector):
         time = tpf.time
         rawflux = lc.flux
         rawflux_err = lc.flux_err
+<<<<<<< HEAD
 
         # create nan mask
         self.nanmask = np.isfinite(time)
@@ -100,18 +101,29 @@ class PLDCorrector(RegressionCorrector):
 
         self.rawflux = self.lc.flux
         self.rawflux_err = self.lc.flux_err
+=======
+>>>>>>> remove gp attempts
 
         # create nan mask
-        self.nanmask = np.isfinite(self.time)
-        self.nanmask &= np.isfinite(self.rawflux)
-        self.nanmask &= np.isfinite(self.rawflux_err)
-        self.nanmask &= np.abs(self.rawflux_err) > 1e-12
+        self.nanmask = np.isfinite(time)
+        self.nanmask &= np.isfinite(rawflux)
+        self.nanmask &= np.isfinite(rawflux_err)
+        self.nanmask &= np.abs(rawflux_err) > 1e-12
+
+        # apply nan mask
+        self.flux = flux[self.nanmask]
+        self.flux_err = flux_err[self.nanmask]
+        self.time = time[self.nanmask]
+        self.rawflux = rawflux[self.nanmask]
+        self.rawflux_err = rawflux_err[self.nanmask]
+        self.lc = lc[self.nanmask]
 
         super(PLDCorrector, self).__init__(lc=self.lc)
 
     def __repr__(self):
         return 'PLDCorrector (LC: {})'.format(self.lc.label)
 
+<<<<<<< HEAD
     @property
     def X(self):
         return self.dm
@@ -203,6 +215,46 @@ class PLDCorrector(RegressionCorrector):
             dm = self._create_higher_order_matrix(dm, order=pld_order, n_pca_terms=n_pca_terms)
 
         self.dm = dm
+=======
+    def create_design_matrix(self, background_mask=None, pixel_components=3,
+                             spline_n_knots=100, spline_degree=3, sparse=False):
+        """Returns a `DesignMatrixCollection`."""
+
+        if background_mask is None:
+            # Default to pixels <1-sigma above the background
+            background_mask = ~self.tpf.create_threshold_mask(1, reference_pixel=None)
+        self.background_mask = background_mask
+
+        DMC, spline = DesignMatrixCollection, create_spline_matrix
+        if sparse:
+            DMC, spline = SparseDesignMatrixCollection, create_sparse_spline_matrix
+        # First, we estimate the per-pixel background flux over time by
+        # (i) subtracting a mean image from each cadence;
+        # (ii) computing the median pixel value in the residual images;
+        # (iii) assume that the 5%-percentile of those medians gives us the
+        # exact background level. This assumption appears to work well for TESS
+        # but it has not been validated in detail yet.
+        simple_bkg = (self.tpf.flux - np.nanmean(self.tpf.flux, axis=0))
+        simple_bkg = np.nanmedian(simple_bkg[:, background_mask], axis=1)
+        simple_bkg -= np.percentile(simple_bkg, 5)
+
+        # Background-corrected pixel time series
+        pixels = (self.tpf.flux.transpose([1, 2, 0]) - simple_bkg
+                    ).transpose([2, 0, 1])[:, background_mask]
+
+        # make sure no columns have nans
+        nanmask = np.isfinite(pixels)
+        zipped = zip(pixels, nanmask)
+        pixels = np.array([p[n] for p,n in zipped])
+
+        dm_pixels = DesignMatrix(pixels, name='pixel_series').pca(pixel_components)
+        dm_bkg = DesignMatrix(simple_bkg, name='background_model')
+        dm_spline = spline(self.lc.time, n_knots=spline_n_knots,
+                             degree=spline_degree).append_constant()
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            dm = DMC([dm_pixels, dm_bkg, dm_spline])
+>>>>>>> remove gp attempts
         return dm
 
     def correct(self, pld_order=1, pixel_components=3, spline_n_knots=100, spline_degree=3,
@@ -238,50 +290,6 @@ class PLDCorrector(RegressionCorrector):
         if restore_trend:
             clc += self.diagnostic_lightcurves['spline']
         return clc
-
-    def _correct_with_gp(self, cadence_mask=None, ridge_value=1e-8, **kwargs):
-        """ """
-
-        try:
-            import celerite
-        except ImportError:
-            log.error("PLD uses the `celerite` Python package. "
-                      "See the installation instructions at "
-                      "https://docs.lightkurve.org/about/install.html. "
-                      "`use_gp` has been set to `False`.")
-
-        X = self.create_design_matrix(**kwargs)
-
-        # set default transit mask
-        if cadence_mask is None:
-            cadence_mask = np.ones_like(self.lc.time, dtype=bool)
-        M = lambda x: x[cadence_mask[self.nanmask]]
-
-        # mask transits in design matrix
-        MX = X.mask(cadence_mask)
-
-        X_gp, gp = MX.apply_gp_inverse(self.lc, ridge_value=ridge_value, return_gp=True,
-                                   cadence_mask=cadence_mask[self.nanmask])
-        y_gp = gp.apply_inverse(M(self.rawflux)[:, None])[:, 0]
-
-        # compute the coefficients C on the basis vectors;
-        # the PLD design matrix will be dotted with C to solve for the noise model.
-        A = np.dot(MX.X.T, X_gp)
-        B = np.dot(MX.X.T, y_gp)
-
-        A[np.diag_indices_from(A)] += ridge_value
-        C = np.linalg.solve(A, B)
-
-        # compute detrended light curve
-        model = np.dot(X.X, C)
-        self.detrended_flux = self.rawflux - (model - np.nanmean(model))
-
-        # Create and return a new LightCurve object with the corrected flux
-        corrected_lc = self.lc.copy()[self.nanmask]
-        corrected_lc.flux = self.detrended_flux
-        corrected_lc.flux_err = self.flux_err
-
-        return corrected_lc
 
     def diagnose(self):
         """Returns diagnostic plots to assess the most recent call to `correct()`.
@@ -320,19 +328,19 @@ class PLDCorrector(RegressionCorrector):
         """Calculate products of columns in `DesignMatrix` and create a new
         `DesignMatrix` for each order of products. Returns a
         `DesignMatrixCollection` with one entry per order.
+
         Returns
         -------
         `.DesignMatrixCollection`
             Design matrix collection with products of columns appended as new columns.
         """
         # higher order design matrices
-        all_dms = [X for X in dm if X.name != 'pixel_series']
-        new_dms = [dm['pixel_series']]
+        new_dms = [dm]
         for i in range(2, order+1):
-            regressors = np.product(list(multichoose(dm['pixel_series'].values.T, order)), axis=1).T
+            regressors = np.product(list(multichoose(dm.values.T, order)), axis=1).T
 
             # make high order design matrix
-            high_order_dm = DesignMatrix(regressors, name=f'PLD Order {i}').standardize()
+            high_order_dm = DesignMatrix(regressors, name=f'PLD Order {i}')
 
             # apply PCA
             if n_pca_terms is not None:
@@ -350,10 +358,7 @@ class PLDCorrector(RegressionCorrector):
 
             new_dms.append(high_order_dm)
 
-        pld_dm = DesignMatrixCollection(new_dms).to_designmatrix(name='pixel_series')
-        all_dms.insert(0, pld_dm)
-
-        return DesignMatrixCollection(all_dms)
+        return DesignMatrixCollection(new_dms)
 
 class TessPLDCorrector(PLDCorrector):
     """
@@ -433,9 +438,3 @@ class KeplerPLDCorrector(PLDCorrector):
         if restore_trend:
             clc += self.diagnostic_lightcurves['spline']
         return clc
-=======
-    def create_design_matrix(self):
-        """ """
-
-        pass
->>>>>>> add option to correct with gp
