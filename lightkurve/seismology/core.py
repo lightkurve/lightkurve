10c8351ce77e2cd968e547d699e530e06e82edf6
Oliver Hall <ojh251@student.bham.ac.uk>
2020-06-25 11:57:48 +0100

    Update lightkurve/seismology/core.py
    Co-authored-by: Geert Barentsen <hello@geert.io>

--- lightkurve/seismology/core.py
+++ lightkurve/seismology/core.py
@@ -665,7 +665,7 @@ class Seismology(object):
         numax = self._validate_numax(numax)
         if teff is None:
             teff = self.periodogram.meta.get('teff')
-            if teff == None:
+            if teff is None:
                 raise ValueError("You must provide an effective temperature argument (`teff`) to `estimate_radius`,"
                         "because the Periodogram object does not contain it in its meta data (i.e. `pg.meta['teff']` is missing")
             else:
