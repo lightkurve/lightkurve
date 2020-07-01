687887fbac5aaf912d615cd0f43857e46d892a52
Geert Barentsen <geert@barentsen.be>
2020-07-01 16:21:05 -0700

    Fix syntax error

--- lightkurve/lightcurve.py
+++ lightkurve/lightcurve.py
@@ -1219,7 +1219,7 @@ class LightCurve(TimeSeries):
                     ylabel = f"{column}"
             if normalize or self.meta.get("normalized"):
                 ylabel = "Normalized " + ylabel
-            elif (self[column].unit) and (self[column].unit.to_string() is not ''):
+            elif (self[column].unit) and (self[column].unit.to_string() != ''):
                 ylabel += f" [{self[column].unit.to_string('latex_inline')}]"
             else: 
                 pass
