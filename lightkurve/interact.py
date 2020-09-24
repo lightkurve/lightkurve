d60494325db2ff60608ce9f2c868181c5f0f0201
Geert Barentsen <geert@barentsen.be>
2020-09-24 12:48:42 -0700

    Use log.warning and log.error instead

--- lightkurve/interact.py
+++ lightkurve/interact.py
@@ -563,19 +563,16 @@ def show_interact_widget(tpf, notebook_url='localhost:8888',
     # https://github.com/bokeh/bokeh/issues/7490
     n_cadences = len(lc.cadenceno)
     if n_cadences > max_cadences:
-        raise RuntimeError(
-            f"Interact cannot display more than {max_cadences} cadences "
-            "without suffering significant performance issues. You can "
-            "limit the number of cadences show using slicing, e.g. "
-            "`tpf[0:1000].interact()`. Alternatively, you can override "
-            "this limitation by passing the `max_cadences` argument."
-        )
+        log.error(f"Error: interact cannot display more than {max_cadences} "
+                  "cadences without suffering significant performance issues. "
+                  "You can limit the number of cadences show using slicing, e.g. "
+                  "`tpf[0:1000].interact()`. Alternatively, you can override "
+                  "this limitation by passing the `max_cadences` argument.")
     elif n_cadences > 30000:
-        warnings.warn(f"The Target Pixel File contains {n_cadences} cadences. "
-                       "The performance of interact() is very slow for such a "
-                       "large number of frames. Consider using slicing, e.g. "
-                       "`tpf[0:1000].interact()`, to make interact run faster.",
-                       LightkurveWarning)
+        log.warning(f"Warning: the pixel file contains {n_cadences} cadences. "
+                    "The performance of interact() is very slow for such a "
+                    "large number of frames. Consider using slicing, e.g. "
+                    "`tpf[0:1000].interact()`, to make interact run faster.")
 
     def create_interact_ui(doc):
         # The data source includes metadata for hover-over tooltips
