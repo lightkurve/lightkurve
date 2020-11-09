0b5dfe1d409c25261f4ba41488bb9c90659b1abb
orionlee <orionlee@yahoo.com>
2020-11-09 11:34:53 -0800

    minor test code style fixes

--- lightkurve/tests/test_collections.py
+++ lightkurve/tests/test_collections.py
@@ -229,13 +229,14 @@ def test_accessor_tess_sector():
     tpfc = TargetPixelFileCollection([tpf, tpf2, tpf3])
     assert((tpfc.sector == [23, None, 1]).all())
 
+
 def test_accessor_kepler_quarter():
     # scaled down version of tess sector test, as they share the same codepath
     lc0 = KeplerLightCurve(time=np.arange(1, 5), flux=np.arange(1, 5),
-                    flux_err=np.arange(1, 5), targetid=50000)
+                           flux_err=np.arange(1, 5), targetid=50000)
     lc0.quarter = 2
     lc1 = KeplerLightCurve(time=np.arange(10, 15), flux=np.arange(10, 15),
-                     flux_err=np.arange(10, 15), targetid=120334)
+                           flux_err=np.arange(10, 15), targetid=120334)
     lc1.quarter = 1
     lcc = LightCurveCollection([lc0, lc1])
     assert((lcc.quarter == [2, 1]).all())
@@ -248,13 +249,14 @@ def test_accessor_kepler_quarter():
     tpfc = TargetPixelFileCollection([tpf0, tpf1])
     assert((tpfc.quarter == [2, 1]).all())
 
+
 def test_accessor_k2_campaign():
     # scaled down version of tess sector test, as they share the same codepath
     lc0 = KeplerLightCurve(time=np.arange(1, 5), flux=np.arange(1, 5),
-                    flux_err=np.arange(1, 5), targetid=50000)
+                           flux_err=np.arange(1, 5), targetid=50000)
     lc0.campaign = 2
     lc1 = KeplerLightCurve(time=np.arange(10, 15), flux=np.arange(10, 15),
-                     flux_err=np.arange(10, 15), targetid=120334)
+                           flux_err=np.arange(10, 15), targetid=120334)
     lc1.campaign = 1
     lcc = LightCurveCollection([lc0, lc1])
     assert((lcc.campaign == [2, 1]).all())
