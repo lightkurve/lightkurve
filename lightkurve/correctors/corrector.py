303c9cbfffeda9d12e56895bacf5397c83c3b6b4
Geert Barentsen <geert@barentsen.be>
2020-11-12 12:30:50 -0800

    :art: Improve definition of the abstract Corrector class.

--- lightkurve/correctors/corrector.py
+++ lightkurve/correctors/corrector.py
@@ -1,17 +1,43 @@
-"""Implements the abstract `Corrector` base class to document the generic
-methods the subclasses should aim to provide.
-
-`Corrector` classes must adopt the following design:
-- `__init__()` takes the required data (e.g. LightCurve, TargetPixelFile);
-- `correct()` takes optional parameters and returns a LightCurve;
-- `diagnose()` creates plots to elucidate the user's most recent call to `correct()`.
+"""Implements the abstract `Corrector` base class.
 """
-class Corrector(object):
-    """Abstract base class."""
-    def correct(self):
-        """Returns a corrected LightCurve."""
-        raise NotImplementedError("This is an abstract base class.")
+from abc import ABC, abstractmethod
 
-    def diagnose(self):
+import matplotlib
+from .. import LightCurve
+
+
+class Corrector(ABC):
+    """Abstract base class documenting the structure of classes intended
+    to remove systematic noise from light curves.
+
+    Attributes
+    ----------
+    lc : LightCurve
+        Original, uncorrected light curve, usually assigned by the constructor.
+    corrected_lc : LightCurve
+        Corrected light curve, assigned by each call to the `correct()` method.
+
+    Methods
+    -------
+    __init__()
+        Accepts all the data required to execute the correction.
+    correct() -> LightCurve
+        Executes the correction, accepting any optional parameters that
+        can be used to modify the way the correction is applied.
+    diagnose() -> matplotlib.axes.Axes
+        Creates plots to elucidate the user's most recent call to `correct()`.
+    """
+
+    lc: LightCurve = None
+    corrected_lc: LightCurve = None
+
+    @abstractmethod
+    def correct(self) -> LightCurve:
+        """Returns a corrected LightCurve, and also stores it as the
+        `Corrector.corrected_lc` attribute."""
+        pass
+
+    @abstractmethod
+    def diagnose(self) -> matplotlib.axes.Axes:
         """Returns plots which elucidate the most recent call to `correct()`."""
-        raise NotImplementedError("This is an abstract base class.")
+        pass
