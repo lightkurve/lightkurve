9e3ef00889f83512c8699a95a54c45d326e250ff
rebekah9969 <rebekahhounsell@gmail.com>
2021-02-04 17:45:21 -0500

    adjustments made to code and removal of null keywords

--- lightkurve/io/tasoc.py
+++ lightkurve/io/tasoc.py
@@ -1,7 +1,7 @@
-"""Reader for A PSF-Based Approach to TESS High Quality Data Of Stellar Clusters (PATHOS) light curve files.
-
-Website: https://archive.stsci.edu/hlsp/pathos
-A product description file wasn't obvious on the MAST website
+"""TESS Asteroseismic Science Operations Center - https://tasoc.dk
+   TESS Data For Asteroseismology Lightcurves - https://archive.stsci.edu/hlsp/tasoc 
+   Data provided with this release have been extracted using the TASOC Photometry pipeline. The TASOC
+   pipeline used to generate the data is open source and available on GitHub - https://github.com/tasoc
 """
 from ..lightcurve import TessLightCurve
 from ..utils import TessQualityFlags
@@ -18,31 +18,20 @@ def read_tasoc_lightcurve(filename,
     ----------
     filename : str
         Local path or remote url of TASOC light curve FITS file.
-    flux_column : 'flux_RAW' - this contains the T'DA extracted lightcurve,
-    with no corrections applied to the raw light curves. Corrected lightcurves 
-    may be a thing in the future as there is a flux_corr column.
-    quality_bitmask : For now this always none - as no calibration applied
+    flux_column : str
+        Column that will be used to populate the flux values.
+        By default, "FLUX_RAW" is used. It contains the T'DA extracted lightcurve,
+        with no corrections applied to the raw light curves. Corrected lightcurves 
+        may become available in the future.
 
     """
     lc = read_generic_lightcurve(filename,
                                  flux_column=flux_column.lower(),
-                                 time_format='btjd',
-                                 quality_column="QUALITY")
-
-    # Filter out poor-quality data
-    # NOTE: Unfortunately Astropy Table masking does not yet work for columns
-    # that are Quantity objects, so for now we remove poor-quality data instead
-    # of masking. Details: https://github.com/astropy/astropy/issues/10119
-    #quality_mask = TessQualityFlags.create_quality_mask(
-    #                            quality_array=lc['dquality'],
-    #                            bitmask=quality_bitmask)
-    #lc = lc[quality_mask]
+                                 time_format='btjd')
+   
 
     lc.meta['TARGETID'] = lc.meta.get('TICID')
-    lc.meta['QUALITY_BITMASK'] = quality_bitmask
-    #lc.meta['QUALITY_MASK'] = quality_mask
-
-    # QLP light curves are normalized by default
+    # TASOC light curves are normalized by default
     lc.meta['NORMALIZED'] = True
 
     return TessLightCurve(data=lc)
