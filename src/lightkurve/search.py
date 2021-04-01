54e458395317b1462c0ba2b7c278db8863b12937
Nicholas Saunders <nksaunders1994@gmail.com>
2021-04-01 10:24:33 -1000

    switch to productFilename

--- src/lightkurve/search.py
+++ src/lightkurve/search.py
@@ -980,7 +980,7 @@ def _search_products(
             # listed separately in the table with suffixes "-1" and "-2"
             if obs_project == "K2" and result["sequence_number"][idx] in [9, 10, 11]:
                 for half,letter in zip([1,2],['a','b']):
-                    if f"c{tmp_seqno}{half}" in result["obs_id"][idx]:
+                    if f"c{tmp_seqno}{half}" in result["productFilename"][idx]:
                         obs_seqno = f"{int(tmp_seqno):02d}{letter}"
             result["mission"][idx] = "{} {} {}".format(
                 obs_project, obs_prefix.get(obs_project, ""), obs_seqno
