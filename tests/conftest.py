f2aeb0c4f5a4496cde57bd2e2b818c91ca810bfe
Sam Lee <orionlee@users.noreply.github.com>
2021-07-09 14:06:46 -0700

    fix lc.bin() excessive memory usage in some cases (#1096)
    * test for lc.bin() excessive memory usage
    
    * :bug: fix lc.bin excessive memory usage (#1092)
    - avoid a bug in astropy 4.1
    
    * make test_bin_memory_usage() obey remote-data convention
    
    * test_bin_memory_usage(): avoid float to int warning
    
    * Add comments for memtest marker
    
    * minor fix for the 3.8-memtest ci run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
+import pytest
+
+
 def pytest_runtest_setup(item):
-    """Our tests will often run in headless virtual environments. For this
+    r"""Our tests will often run in headless virtual environments. For this
     reason, we enforce the use of matplotlib's robust Agg backend, because it
     does not require a graphical display.
 
@@ -10,3 +13,28 @@ def pytest_runtest_setup(item):
     import matplotlib
 
     matplotlib.use("Agg")
+
+# Add a marker @pytest.mark.memtest
+# - used to mark tests that stress memory, typically done by limiting the memory Python can use
+# - thus they should be run in isolation.
+#
+# - skipped by default
+# - tests marked as such can be run by "-m memtest" option
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "memtest: mark memory usage tests that need to be run in isolation"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    keywordexpr = config.option.keyword
+    markexpr = config.option.markexpr
+    if keywordexpr or markexpr:
+        return  # let pytest handle this
+
+    skip_memtest = pytest.mark.skip(reason='memtest skipped, need -m memtest option to run')
+    for item in items:
+        if 'memtest' in item.keywords:
+            item.add_marker(skip_memtest)
