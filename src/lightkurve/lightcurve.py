85f2e188842f94647d5a7c13a7a3593eac499a4c
Benjamin Pope <bjspope@gmail.com>
2021-07-27 17:02:46 +1000

    hasattr vs dir

--- src/lightkurve/lightcurve.py
+++ src/lightkurve/lightcurve.py
@@ -2310,7 +2310,7 @@ class LightCurve(QTimeSeries):
                         name=flux_column_name, format="E", unit="e-/s", array=self.flux
                     )
                 )
-            if "flux_err" in dir(self):
+            if hasattr(self,'flux_err'):
                 if ~(flux_column_name.upper() + "_ERR" in extra_data.keys()):
                     cols.append(
                         fits.Column(
@@ -2320,7 +2320,7 @@ class LightCurve(QTimeSeries):
                             array=self.flux_err,
                         )
                     )
-            if "cadenceno" in dir(self):
+            if hasattr(self,'cadenceno'):
                 if ~np.asarray(
                     ["CADENCENO" in k.upper() for k in extra_data.keys()]
                 ).any():
