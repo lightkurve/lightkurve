30919aa690ac63de36e7b3cf95e680fa262166ee
Keyu Xing <kyxing@mail.bnu.edu.cn>
2022-04-16 00:52:41 +0800

    Fix flux unit of flatten light curve (fixes #630) (#1195)
    * Fix flux unit of flatten light curve (#630)
    
    * Add NORMALIZED header to flatten light curve
    
    * Add test for NORMALIZED header
    
    Add test for NORMALIZED header to lc.normalize() and lc.flatten()
    
    * Add change log entry for #1195

--- tests/test_lightcurve.py
+++ tests/test_lightcurve.py
@@ -721,6 +721,7 @@ def test_normalize():
     with pytest.warns(None) as warn_record:
         lc.normalize()
     assert len(warn_record) == 0
+    assert lc.meta["NORMALIZED"]
 
 
 def test_invalid_normalize():
@@ -1021,8 +1022,10 @@ def test_flatten_returns_normalized():
     )
     flat_lc, trend_lc = lc.flatten(window_length=3, polyorder=1, return_trend=True)
 
-    assert flat_lc.flux.unit is lc_flux_unit
-    assert flat_lc.flux_err.unit is lc_flux_unit
+    assert flat_lc.flux.unit == u.dimensionless_unscaled
+    assert flat_lc.flux_err.unit == u.dimensionless_unscaled
+    assert flat_lc.meta["NORMALIZED"]
+    
     assert trend_lc.flux.unit is lc_flux_unit
     assert trend_lc.flux_err.unit is lc_flux_unit
 
