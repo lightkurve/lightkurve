e55f6fc7bd79f4b547ac154dfb02524b6c76aeb6
orionlee <orionlee@yahoo.com>
2022-04-22 15:54:14 -0700

    Fix broken TASOC test due to DR5 changes

--- tests/test_units.py
+++ tests/test_units.py
@@ -3,6 +3,7 @@ import pytest
 import lightkurve as lk  # necessary to enable the units tested below
 from astropy import units as u
 
+from .io.test_tasoc import TEST_TIC_ID
 
 def test_custom_units():
     """Are ppt, ppm, and percent enabled AstroPy units?"""
@@ -14,6 +15,7 @@ def test_custom_units():
 @pytest.mark.remote_data
 def test_tasoc_ppm_units():
     """Regression test for #956."""
-    lc = lk.search_lightcurve('HV 2112', author='TASOC', sector=1, exptime=1800).download()
+
+    lc = lk.search_lightcurve(f"TIC {TEST_TIC_ID}", author='TASOC').download()
     assert lc['flux_corr'].unit == "ppm"
     assert "Unrecognized" not in repr(lc['flux_corr'].unit)
