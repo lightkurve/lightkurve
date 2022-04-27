1b0d63185d9471a0ae52e356a8e7374ded57543f
Sam Lee <orionlee@users.noreply.github.com>
2022-04-27 13:27:46 -0700

    Fix broken TASOC test due to DR5 changes (#1201)

--- tests/io/test_tasoc.py
+++ tests/io/test_tasoc.py
@@ -9,10 +9,15 @@ from lightkurve.io.tasoc import read_tasoc_lightcurve
 from lightkurve.io.detect import detect_filetype
 
 
+# The URL needs to be updated upon a new TASOC data release.
+TEST_TIC_ID = 150441810
+TEST_FIT_URL = "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:HLSP/tasoc/s0001/c1800/0000/0001/5044/1810/hlsp_tasoc_tess_ffi_tic00150441810-s0001-cam4-ccd4-c1800_tess_v05_ens-lc.fits"
+
+
 @pytest.mark.remote_data
 def test_detect_tasoc():
     """Can we detect the correct format for TASOC files?"""
-    url = "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:HLSP/tasoc/s0001/ffi/0000/0004/1206/4070/hlsp_tasoc_tess_ffi_tic00412064070-s01-c1800_tess_v04_lc.fits"
+    url = TEST_FIT_URL
     f = fits.open(url)
 
     assert detect_filetype(f) == "TASOC"
@@ -21,7 +26,7 @@ def test_detect_tasoc():
 @pytest.mark.remote_data
 def test_read_tasoc():
     """Can we read TASOC files?"""
-    url = "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:HLSP/tasoc/s0001/ffi/0000/0004/1206/4070/hlsp_tasoc_tess_ffi_tic00412064070-s01-c1800_tess_v04_lc.fits"
+    url = TEST_FIT_URL
     with fits.open(url, mode="readonly") as hdulist:
         fluxes = hdulist[1].data["FLUX_RAW"]
 
@@ -34,9 +39,8 @@ def test_read_tasoc():
 @pytest.mark.remote_data
 def test_search_tasoc():
     """Can we search and download a TASOC light curve?"""
-    search = search_lightcurve("TIC 412064070", author="TASOC", sector=1)
-    assert len(search) == 1
+    search = search_lightcurve(f"TIC {TEST_TIC_ID}", author="TASOC")
+    assert len(search) >= 1
     assert search.table["author"][0] == "TASOC"
     lc = search.download()
     assert type(lc).__name__ == "TessLightCurve"
-    assert lc.sector == 1
