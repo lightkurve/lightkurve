0a79fe17ef15bb45d4942ef4a6c644e568db6c0c
orionlee <orionlee@yahoo.com>
2022-05-17 13:02:48 -0700

    Test: search result display column customization

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -12,13 +12,22 @@ def test_read_conf_from_file():
     assert [] == lk.conf.search_result_display_extra_columns
     # Use a custom config file, and assert the changes are read.
     try:
-        cfg_dest_path = Path(lk.config.get_config_dir(), 'lightkurve.cfg')
-        cfg_src_path = get_pkg_data_filename("data/lightkurve_sr_cols_added.cfg")
-        shutil.copy(cfg_src_path, cfg_dest_path)
-        lk.conf.reload()
+        use_custom_config_file("data/lightkurve_sr_cols_added.cfg")
         assert ['proposal_id'] == lk.conf.search_result_display_extra_columns
     finally:
         # cleanup: remvoe the custom config file and its effect
-        cfg_dest_path.unlink()
-        lk.conf.reload()
+        remove_custom_config()
 
+
+def use_custom_config_file(cfg_filepath):
+    """Copy the config file in the given path (in tests) to the default lightkurve config file """
+    cfg_dest_path = Path(lk.config.get_config_dir(), 'lightkurve.cfg')
+    cfg_src_path = get_pkg_data_filename(cfg_filepath)
+    shutil.copy(cfg_src_path, cfg_dest_path)
+    lk.conf.reload()
+
+
+def remove_custom_config():
+    cfg_dest_path = Path(lk.config.get_config_dir(), 'lightkurve.cfg')
+    cfg_dest_path.unlink()
+    lk.conf.reload()
