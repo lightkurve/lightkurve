21af634afae4b9f9fc7d98bd863de793eba4910d
vbkostov <veselin.b.kostov@gmail.com>
2022-06-27 22:48:58 +0300

    Update test_eleanorlite.py

--- tests/io/test_eleanorlite.py
+++ tests/io/test_eleanorlite.py
@@ -11,7 +11,9 @@ from lightkurve.io.detect import detect_filetype
 @pytest.mark.remote_data
 def test_eleanor_lite():
     """Can we read in GSFC-ELEANOR-LITE light curves?"""
-    url = "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:HLSP/gsfc-eleanor-lite/s0001/0000/0003/3673/2616/hlsp_gsfc-eleanor-lite_tess_ffi_s0001-0000000336732616_tess_v1.0_lc.fits" 
+    url =
+    "https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:HLSP/gsfc-eleanor-lite/s0001/0000/"
+    "0003/3673/2616/hlsp_gsfc-eleanor-lite_tess_ffi_s0001-0000000336732616_tess_v1.0_lc.fits"
     with fits.open(url, mode="readonly") as hdulist:
         # Can we auto-detect a GSFC-ELEANOR-LITE file?
         assert detect_filetype(hdulist) == "GSFC-ELEANOR-LITE"
@@ -20,7 +22,7 @@ def test_eleanor_lite():
         assert lc.meta["FLUX_ORIGIN"] == "corr_flux"
         assert_array_equal(lc.flux.value, hdulist[1].data["CORR_FLUX"])
         # Are the correct quality flags read in?
-        lc = read_eleanorlite_lightcurve(url, quality_bitmask='default')        
+        lc = read_eleanorlite_lightcurve(url, quality_bitmask='default')
         assert ((lc["quality"] & 2**17) != 0).any() and ((lc["quality"] & 2**18) != 0).any()
         lc = read_eleanorlite_lightcurve(url, quality_bitmask='hardest')
         assert not (lc["quality"] & (2**17 | 2**18)).any()
