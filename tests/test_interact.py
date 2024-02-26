04abe6705ae0e085dff4726d80ced413a2b0d142
orionlee <orionlee@yahoo.com>
2024-02-26 10:36:22 -0800

    barebone test (to ensure there is no syntax error) - ful test requires interaction that needs to be done manually.

--- tests/test_interact.py
+++ tests/test_interact.py
@@ -62,6 +62,19 @@ def test_graceful_exit_outside_notebook():
 
 
 @pytest.mark.skipif(bad_optional_imports, reason="requires bokeh")
+def test_return_selected_mask():
+    """Test to `return_selected_mask=True` can run without any syntax error."""
+    import bokeh
+
+    tpf = TessTargetPixelFile(example_tpf)
+    mask_to_use = tpf.create_threshold_mask()
+    result, selection_mask = tpf.interact(aperture_mask=mask_to_use, return_selected_mask=True)
+    assert result is None
+    # the returned mask should be the same as the supplied one initially
+    assert_array_equal(selection_mask, mask_to_use)
+
+
+@pytest.mark.skipif(bad_optional_imports, reason="requires bokeh")
 def test_custom_aperture_mask():
     """Can we provide a custom lightcurve to show?"""
     with warnings.catch_warnings():
