c44d92d515590384059d069174865d64a5aeb083
Derek Homeier <dhomeier@users.noreply.github.com>
2024-07-18 01:06:48 +0200

    Make `oktopus` optional dependency

--- src/lightkurve/prf/__init__.py
+++ src/lightkurve/prf/__init__.py
@@ -1,2 +1,6 @@
+import warnings
 from .prfmodel import *
-from .tpfmodel import *
+try:
+    from .tpfmodel import *
+except ModuleNotFoundError:
+    warnings.warn("Warning: the tpfmodel module is not available without oktopus")
